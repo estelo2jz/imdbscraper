96ddef72dd4472780aa4dbbb5058633d049ca671
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-07-16 07:48:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+const sompleResult = {
+  title: 'Hamilton',
+  rank: 1,
+  imdbRating: 9.0,
+  descriptionUrl: 'https://www.imdb.com/title/tt8503618/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=ea4e08e1-c8a3-47b5-ac3a-75026647c16e&pf_rd_r=M5MFF5MSZQE528NE18GZ&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_tt_1',
+  posterUrl: 'https://www.imdb.com/title/tt8503618/mediaviewer/rm1749789441?ref_=tt_ov_i'
+}
+
+async function main() {
+  
+}
+
+main();
